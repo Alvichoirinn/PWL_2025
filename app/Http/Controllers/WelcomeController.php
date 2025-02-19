a6a82009c69516925e19b5cc6e04dfae64b5635b
Alvi Choirinnikmah <avialvi08@gmail.com>
2025-02-19 16:12:28 +0700

    Percobaan 3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,5 +9,14 @@ class WelcomeController extends Controller
     public function hello() {
         return 'Hello World';
     }
+    // public function greeting(){
+    //     return view('blog.hello', ['name' => 'Alvi Choirinnikmah']);
+    // }
+    public function greeting(){
+        return view('blog.hello')
+            ->with('name','Alvi Choirinnikmah')
+            ->with('occupation','Astronaut');
+    }
+
 }
 
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PhotoController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -83,4 +84,10 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+
+Route::get('/greeting', function () {
+	return view('blog.hello', ['name' => 'Alvi Choirinnikmah']);
+});
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
 
